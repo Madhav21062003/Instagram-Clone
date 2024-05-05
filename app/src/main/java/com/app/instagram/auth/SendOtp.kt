a987741c3ad2945399cb413d43ba448237a801fe
weather <sanjaysangeetajain@gmail.com>
2024-05-05 22:14:24 +0530

    my first commit

---
+++ app/src/main/java/com/app/instagram/auth/SendOtp.kt
@@ -0,0 +1,149 @@
+package com.app.instagram.auth
+
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.app.instagram.R
+import com.app.instagram.fragments.HomeFragment
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class SendOtp : AppCompatActivity() {
+
+    private lateinit var getOTPBtn: Button
+    private lateinit var phoneNumber: EditText
+    private lateinit var auth:FirebaseAuth
+    private lateinit var number:String
+    //private lateinit var text: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_otp)
+
+        init()
+
+        getOTPBtn.setOnClickListener{
+            number = phoneNumber.text.trim().toString()
+            if(number.isNotEmpty()){
+                if(number.length==10){
+                    number="+91$number"
+
+                    val options = PhoneAuthOptions.newBuilder(auth)//Pass the authentication
+                        .setPhoneNumber(number) // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this) // Activity (for callback binding)
+                        .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+                }else{
+                    Toast.makeText(this,"Please enter the correct number",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Please enter number",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+    private fun init(){
+        getOTPBtn=findViewById(R.id.button)
+        phoneNumber=findViewById(R.id.input_mobile)
+        //text=findViewById(R.id.text_mobile_no)
+
+        auth=FirebaseAuth.getInstance()
+    }
+
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            Log.d(TAG, "onVerificationCompleted:$credential")
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                // reCAPTCHA verification attempted with null Activity
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken,
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Log.d(TAG, "onCodeSent:$verificationId")
+            val intent= Intent(this@SendOtp, verification::class.java)
+            intent.putExtra("OTP",verificationId)
+            intent.putExtra("resendToken",token)
+            intent.putExtra("phoneNumber",number)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(auth.currentUser!=null){ //User is authenticated
+            startActivity(Intent(this@SendOtp, HomeFragment::class.java))
+        }
+    }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    Toast.makeText(this,"Authenticate successfully",Toast.LENGTH_SHORT).show()
+                    sendToMain()
+
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure:${ task.exception.toString()}")
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                }
+            }
+    }
+    private fun sendToMain(){
+        startActivity(Intent(this@SendOtp, HomeFragment::class.java))
+    }
+
+
+}
